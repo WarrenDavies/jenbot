49b62b4bc1c75010b417f1225c8aac478b6d9298
Warren Davies <warren_davies81@hotmail.com>
2026-01-28 20:29:09 +0000

    added open-meteo API call in WeatherTool

--- src/jenbot/chat/response_generator.py
+++ src/jenbot/chat/response_generator.py
@@ -4,13 +4,15 @@ import textwrap
 class ResponseGenerator:
 
     def create_tool_use_prompt(self, tool_response):
-        content = f"""
-        TOOL USE RESULT 
+        content = f"""TOOL USE RESULT 
         Tool name: {tool_response["name"]}
         Status: {tool_response["status"]} 
         Output: {tool_response["output"]}
+                
+        Include the details of the tool use in your response in a natural way to answer the user's previous query."""
         
-        Add the tool output to your response in a natural way to answer the user's previous query."""
+        content_stripped = [line.lstrip() for line in content.splitlines()]
+        content = '\n'.join(content_stripped)
         content = textwrap.dedent(content)
 
         system_prompt = {"role": "system", "content": content}
