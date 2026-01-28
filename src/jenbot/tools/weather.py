651a2955769496d7f8887dd6019e4dddae61e498
Warren Davies <5620086+WarrenDavies@users.noreply.github.com>
2026-01-28 20:29:58 +0000

    Feature/add weather tool (#8)
    * added open-meteo API call in WeatherTool

--- src/jenbot/tools/weather.py
+++ src/jenbot/tools/weather.py
@@ -1,5 +1,6 @@
-from jenbot.tools.registry import register
+import requests
 
+from jenbot.tools.registry import register
 
 
 @register("weather")
@@ -10,8 +11,37 @@ class WeatherTool:
 
 
     def run(self, parameters):
+        current = ["temperature_2m", "apparent_temperature", "is_day", "rain", "weather_code", "wind_speed_10m", "cloud_cover", "showers", "precipitation"]
+        base_url = "https://api.open-meteo.com/v1/forecast"
+        params = {
+            "latitude": 51.53,
+            "longitude": -0.14,
+            "current": current,
+            "forecast_days": 1,
+        }
+
+        response = None
+        try:
+            response = requests.get(base_url, params=params)
+            response.raise_for_status()
+        except requests.exceptions.RequestException as e:
+            return {
+                "name": self.name,
+                "status": "failure",
+                "output": f"API request failed with exception: {e}"
+            }
+
+        data = response.json()
+        
+        metrics = []
+        for metric in current:
+            metrics.append(
+                f"""{metric}: {data["current"][metric]} {data["current_units"][metric]}"""
+            )
+        metrics_str = "\n".join(metrics)
+
         return {
             "name": self.name,
             "status": "success",
-            "output": f"The weather outside is frightful, but the fire is so delightful"
+            "output": metrics_str
         }
