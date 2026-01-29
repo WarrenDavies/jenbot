daf47ebba6c91e545e51a83fbf04fbed7a0f14a6
Warren Davies <warren_davies81@hotmail.com>
2026-01-29 10:47:27 +0000

    added BaseBot and Jenbot

---
+++ src/jenbot/bots/base/base_bot.py
@@ -0,0 +1,32 @@
+from abc import ABC, abstractmethod
+
+
+
+class BaseBot(ABC):
+
+    def __init__(self, config):
+        self.config = config
+
+    
+    def create_tool_use_prompt(self, tool_response):
+        content = f"""TOOL USE RESULT 
+        Tool name: {tool_response["name"]}
+        Status: {tool_response["status"]} 
+        Output: {tool_response["output"]}
+                
+        Include the details of the tool use in your response in a natural way to answer the user's previous query."""
+        
+        content_stripped = [line.lstrip() for line in content.splitlines()]
+        content = '\n'.join(content_stripped)
+        content = textwrap.dedent(content)
+
+        tool_use_system_prompt = {"role": "system", "content": content}
+
+        return tool_use_system_prompt
+
+
+    @abstractmethod
+    def generate_response(self, prompt):
+
+        pass
+    
---
+++ src/jenbot/bots/jenbot/jenbot.py
@@ -0,0 +1,40 @@
+from abc import ABC, abstractmethod
+
+from jenbot.bots.base.base_bot import BaseBot
+
+
+class Jenbot(BaseBot):
+
+
+    def __init__(self, config):
+        __super__
+        self.config = config
+
+    
+    def create_tool_use_prompt(self, tool_response):
+        content = f"""TOOL USE RESULT 
+        Tool name: {tool_response["name"]}
+        Status: {tool_response["status"]} 
+        Output: {tool_response["output"]}
+                
+        Include the details of the tool use in your response in a natural way to answer the user's previous query."""
+        
+        content_stripped = [line.lstrip() for line in content.splitlines()]
+        content = '\n'.join(content_stripped)
+        content = textwrap.dedent(content)
+
+        tool_use_system_prompt = {"role": "system", "content": content}
+
+        return tool_use_system_prompt
+
+
+    @abstractmethod
+    def generate_response(self, prompt):
+
+        reponse = "beep beep boop boop I'm replying"
+        
+        return {
+            "role": "assistant",
+            "content": reponse
+        }
+        
