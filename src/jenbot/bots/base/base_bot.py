ca8a65170c96fb956622479bd12978afd74c5f8b
Warren Davies <warren_davies81@hotmail.com>
2026-01-29 11:12:58 +0000

    removed .create_tool_use_prompt() from Jenbot

--- src/jenbot/bots/base/base_bot.py
+++ src/jenbot/bots/base/base_bot.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-
+import textwrap
 
 
 class BaseBot(ABC):
--- src/jenbot/bots/jenbot/jenbot.py
+++ src/jenbot/bots/jenbot/jenbot.py
@@ -12,23 +12,6 @@ class Jenbot(BaseBot):
         self.config = config
 
     
-    def create_tool_use_prompt(self, tool_response):
-        content = f"""TOOL USE RESULT 
-        Tool name: {tool_response["name"]}
-        Status: {tool_response["status"]} 
-        Output: {tool_response["output"]}
-                
-        Include the details of the tool use in your response in a natural way to answer the user's previous query."""
-        
-        content_stripped = [line.lstrip() for line in content.splitlines()]
-        content = '\n'.join(content_stripped)
-        content = textwrap.dedent(content)
-
-        tool_use_system_prompt = {"role": "system", "content": content}
-
-        return tool_use_system_prompt
-
-
     def generate(self, prompt):
 
         reponse = "beep beep boop boop I'm replying"
