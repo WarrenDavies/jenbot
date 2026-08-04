6b98c872fb34cbde8e462c3f123e49eebf408448
Warren Davies <5620086+WarrenDavies@users.noreply.github.com>
2026-08-04 20:01:06 +0100

    Feature/swappable bots (#21)
    * added swappable bots

--- src/jenbot/bots/interview_bot.py
+++ src/jenbot/bots/interview_bot.py
@@ -6,10 +6,12 @@ import random
 
 from speechjenerator.registry import get_model_class
 
-from jenbot.bots.base.base_bot import BaseBot
+from jenbot.bots.base_bot import BaseBot
 from jenbot.memory.memory_manager import MemoryManager
+from jenbot.bots.registry import register
 
 
+@register("interviewbot")
 class InterviewBot(BaseBot):
 
 
