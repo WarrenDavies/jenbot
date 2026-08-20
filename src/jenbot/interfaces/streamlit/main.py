d0820a737e08366df1a8f338680134a50df2b60e
Warren Davies <warren_davies81@hotmail.com>
2026-08-20 21:44:23 +0100

    added mvp streamlit app

--- src/jenbot/interfaces/streamlit/main.py
+++ src/jenbot/interfaces/streamlit/main.py
@@ -1,20 +1,51 @@
+import yaml
+
 import streamlit as st
 
-st.title("Jenbot")
+from audiorecorder.audio_recorder import AudioRecorder
+from sttjenerator.models import registry
 
-# Initialize chat history
+from jenbot.core.orchestrator import Orchestrator
+
+
+### config
+with open("configs/core.yaml", 'r') as stream:
+    core_config = yaml.safe_load(stream)
+
+st.session_state.conversation_id = "test1234"
+number_of_messages = 10
+
+with open("configs/interface.yaml", 'r') as stream:
+    interface_config = yaml.safe_load(stream)
+
+
+### set up
+
+@st.cache_resource
+def load_orchestrator(core_config):
+    return Orchestrator(core_config)
+orchestrator = load_orchestrator(core_config)
+
+
+
 if "messages" not in st.session_state:
-    st.session_state.messages = []
+    messages = orchestrator.bot.memory_manager.get_recent_messages(
+        st.session_state.conversation_id,
+        number_of_messages
+    )
+    if not messages:
+        messages = []
+    st.session_state.messages = messages
 
 
-with st.chat_message("user"):
-    st.write("Hello 👋")
 
-with st.chat_message("assistant"):
-    st.write("Hello human")
+### render
 
-
-
+st.title("Jenbot")
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
 
 
 
@@ -24,17 +55,20 @@ if prompt := st.chat_input("Say something"):
         "content": prompt,
     })
 
-    ## get bot response
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    ### process input
+    payload = {
+        "conversation_id": st.session_state.conversation_id, 
+        "content": prompt
+    }
+    response = orchestrator.process(payload)
     st.session_state.messages.append({
         "role": "assistant",
-        "content": prompt,
+        "content": response,
     })
 
+    with st.chat_message("assistant"):
+        st.markdown(response)
 
-
-
-
-# Display chat messages from history on app rerun
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
