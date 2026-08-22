34130964c549d3f50b13341549fff00e0bc1a116
Warren Davies <5620086+WarrenDavies@users.noreply.github.com>
2026-08-22 11:13:29 +0100

    Feature/streamlit new chat (#30)
    * added new chat button
    
    * updated chat list to move new and updated chats to the top of the list

--- src/jenbot/interfaces/streamlit/main.py
+++ src/jenbot/interfaces/streamlit/main.py
@@ -1,5 +1,6 @@
 import yaml
 import uuid
+import datetime
 
 import streamlit as st
 
@@ -17,10 +18,12 @@ with open("configs/interface.yaml", 'r') as stream:
     interface_config = yaml.safe_load(stream)
 
 if "active_conversation_id" not in st.session_state:
-    st.session_state.active_conversation_id = "test1234"
+    st.session_state.active_conversation_id = str(uuid.uuid4())
 
 if "requested_conversation_id" not in st.session_state:
-    st.session_state.requested_conversation_id = "test1234"
+    st.session_state.requested_conversation_id = st.session_state.active_conversation_id
+
+
 
 number_of_messages = 100
 
@@ -32,6 +35,10 @@ def load_orchestrator(core_config):
     return Orchestrator(core_config)
 orchestrator = load_orchestrator(core_config)
 
+if "conversations" not in st.session_state:
+    st.session_state.conversations = (
+        orchestrator.bot.memory_manager.get_conversation_ids()
+    )
 
 conversation_changed = (
     st.session_state.requested_conversation_id != st.session_state.active_conversation_id
@@ -51,10 +58,6 @@ if ("messages" not in st.session_state) or conversation_changed:
 
 
 
-if "conversations" not in st.session_state:
-    st.session_state.conversations = (
-        orchestrator.bot.memory_manager.get_conversation_ids()
-    )
 
 
 
@@ -65,15 +68,24 @@ st.title("Jenbot")
 
 with st.sidebar:
 
+    st.markdown("## Jenbot")
+
+    if st.sidebar.button("New Chat", key=f"New Chat", use_container_width=True):
+        st.session_state.requested_conversation_id = str(uuid.uuid4())
+        st.rerun()
+
     st.markdown("### 💬 Chats")
 
     conversation_text = st.empty()
+
     for conversation in st.session_state.conversations:
         conversation_id = conversation["conversation_id"]
         count = conversation["message_count"]
         is_active = st.session_state.active_conversation_id == conversation_id
 
         label = f"{'▶ ' if is_active else ''}{conversation_id} ({count})"
+
+
 
         if st.sidebar.button(label, key=f"btn_{conversation_id}", use_container_width=True):
             st.session_state.requested_conversation_id = conversation_id
@@ -112,4 +124,30 @@ if prompt := st.chat_input("Say something"):
 
     with st.chat_message("assistant"):
         st.markdown(response)
+
+    if st.session_state.active_conversation_id not in (
+        [conversation["conversation_id"] for conversation in st.session_state.conversations]
+    ):
+        st.session_state.conversations.insert(
+            0,
+            {
+                "conversation_id": st.session_state.active_conversation_id,
+                "latest_message_ts": datetime.datetime.now().strftime("%Y%m%d%H%M%S"),
+                "message_count": 2,
+            }
+        )   
+    else: 
+        for conversation in st.session_state.conversations:
+            if conversation["conversation_id"] == st.session_state.active_conversation_id:
+                conversation["message_count"] += 2
+                conversation["latest_message_ts"] = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+                break
+
+        if st.session_state.active_conversation_id != st.session_state.conversations[0]["conversation_id"]:
+            st.session_state.conversations = sorted(
+                st.session_state.conversations, 
+                key=lambda x: x['latest_message_ts'],
+                reverse=True,
+            )
+    
 
